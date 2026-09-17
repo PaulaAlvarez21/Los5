1b5de58945362ecf3d079a079d692f3aedb6fab9
Guadalupe Hermida <ghermida@alumnos.exa.unicen.edu.ar>
2026-09-17 11:48:13 -0300

    Estructura de capa lógica

---
+++ pkg/repositorios/reservaRepository.go
@@ -0,0 +1,15 @@
+package repositorios
+
+/*
+	"database/sql" //Interfaz estándar de Go para bases de datos SQL
+	"log"
+	"os"
+	"testing" //Paquete de go para pruebas
+
+	sqlc "Los5/db/sqlc"
+
+	_ "github.com/jackc/pgx/v5/stdlib"*/
+
+type Repository interface {
+	//Crud que obtenga de la base
+}
