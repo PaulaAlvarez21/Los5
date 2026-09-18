0c8620672d9d44f95689db8dbdad21242bb7f866
Guadalupe Hermida <ghermida@alumnos.exa.unicen.edu.ar>
2026-09-17 22:35:01 -0300

    Se actualizó reservaHandler y se propusieron en comentarios posibles opciones para conectar capas

--- pkg/handlers/reservaHandlers.go
+++ pkg/handlers/reservaHandlers.go
@@ -12,10 +12,35 @@ type ReservaHandler struct {
 	reservaService *logica.ReservaService
 }
 
-func CrearReservaHandler(w http.ResponseWriter, r *http.Request) {
-
+func newReservaHandler(reservaService *logica.ReservaService) *ReservaHandler {
+	return &ReservaHandler{reservaService: reservaService}
 }
 
+// Crear una reserva
+func (h *ReservaHandler) CrearReserva(w http.ResponseWriter, r *http.Request) {
+
+	var reserva logica.CrearReservaRequest
+
+	err := json.NewDecoder(r.Body).Decode(&reserva)
+
+	if err != nil {
+		http.Error(w, "JSON inválido", http.StatusBadRequest)
+		return
+	}
+
+	reservaCreada, err := h.reservaService.CrearReserva(reserva)
+
+	if err != nil {
+		http.Error(w, "Error al crear la reserva", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(reservaCreada)
+}
+
+// obtener una reserva con ID
 func (h *ReservaHandler) ObtenerReservasHandler(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.Path, "/")
@@ -36,8 +61,60 @@ func (h *ReservaHandler) ObtenerReservasHandler(w http.ResponseWriter, r *http.R
 	json.NewEncoder(w).Encode(reserva)
 }
 
-func ActualizarReservaHandler(w http.ResponseWriter, r *http.Request) {
+// Obtener todas las reservas
+func (h *ReservaHandler) ObtenerReservas(w http.ResponseWriter, r *http.Request) {
+
+	reservas, err := h.reservaService.ObtenerReservas()
+
+	if err != nil {
+		http.Error(w, "Error al obtener las reservas", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(reservas)
 }
 
-func EliminarReservaHandler(w http.ResponseWriter, r *http.Request) {
+// Actualizar una reserva
+
+func (h *ReservaHandler) ActualizarReserva(w http.ResponseWriter, r *http.Request, id int) {
+
+	var reserva logica.ReservaActualizar //en services hay que crear un struct para actualizar reserva, porque no se puede usar el mismo que para crear reserva, ya que no tiene el ID
+
+	err := json.NewDecoder(r.Body).Decode(&reserva)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	reserva.IDReserva = int32(id)
+
+	reservaActualizada, err := h.reservaService.ActualizarReserva(reserva) //en services hay que crear un struct para actualizar reserva, porque no se puede usar el mismo que para crear reserva, ya que no tiene el ID
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(reservaActualizada)
+}
+
+// Eliminar una reserva
+func (h *ReservaHandler) EliminarReserva(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest)
+		return
+	}
+
+	err = h.reservaService.EliminarReserva(id)
+
+	if err != nil {
+		http.Error(w, "Error al eliminar la reserva", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
--- pkg/logica/reservaService.go
+++ pkg/logica/reservaService.go
@@ -10,6 +10,18 @@ type ReservaService struct {
 	reservaRepository *repositorios.ReservasRepository
 }
 
+//hay que crear el struct de request para crear reserva y actualizar reserva, porque no se puede usar el struct de sqlc porque tiene el id que no se necesita al crear y al actualizar se necesita el id pero no se necesita el id_depto ni la fecha_inicio ni la fecha_fin
+/*
+type ReservaActualizar struct {
+    IDReserva     int32
+    FechaInicio   time.Time
+    IDDepto       int32
+    FechaFin      time.Time
+    PrecioBase    string
+    CantNoches    int32
+    Descuento     sql.NullString
+    Observaciones sql.NullString
+}*/
 // constructor
 func NewReservaService(reservaRepository *repositorios.ReservasRepository) *ReservaService {
 	return &ReservaService{reservaRepository: reservaRepository}
@@ -25,4 +37,11 @@ func (s *ReservaService) ObtenerReservas() ([]repositorios.Reserva, error) {
 
 func (s *ReservaService) ObtenerReserva(id int32) (repositorios.Reserva, error) {
 	return s.reservaRepository.GetReserva(context.Background(), id)
+}
+func (s *ReservaService) ActualizarReserva(reserva repositorios.UpdateReserva) error { //(reserva ReservaActualizar) error {
+	return s.reservaRepository.UpdateReserva(context.Background(), reserva)
+}
+
+func (s *ReservaService) EliminarReserva(id int32) error {
+	return s.reservaRepository.DeleteReserva(context.Background(), id)
 }
